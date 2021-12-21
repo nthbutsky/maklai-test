d431c2ea0531fad43684340afeb0adcc275e1984
Denis Butsky <nthbutsky@gmail.com>
2021-12-21 23:50:26 +0200

    add logic

---
+++ src/js/index.js
@@ -0,0 +1,39 @@
+const firstOperand = document.querySelector('#input-1');
+const secondOperand = document.querySelector('#input-2');
+const operator = document.querySelector('#select');
+const submit = document.querySelector('#submit');
+const result = document.querySelector('#result');
+
+submit.addEventListener('click', calculate);
+
+function calculate(event) {
+  event.preventDefault();
+  let calcResult;
+  switch (operator.value) {
+    case '+':
+      calcResult = +firstOperand.value + +secondOperand.value;
+      break;
+    case '-':
+      calcResult = +firstOperand.value - +secondOperand.value;
+      break;
+    case '*':
+      calcResult = +firstOperand.value * +secondOperand.value;
+      break;
+    case '/':
+      calcResult = +firstOperand.value / +secondOperand.value;
+      break;
+    case '':
+      calcResult = null;
+      break;
+  }
+
+  if (calcResult) {
+    result.innerHTML = Math.trunc(calcResult);
+  } else {
+    result.innerHTML = 'Please, choose an operator!';
+  }
+
+  firstOperand.value = '';
+  secondOperand.value = '';
+  operator.value = '';
+}
